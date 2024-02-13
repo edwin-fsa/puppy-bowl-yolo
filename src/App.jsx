dac95f6c987e50633fadda4267fbc39031c396ff
Edwin Marshall <edwin.marshall@fullstackacademy.com>
2024-02-12 19:55:13 -0800

    Added more functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useState, useEffect } from "react";
+import { getPlayers, getPlayer, deletePlayer, createPlayer } from "./api";
+import { Player } from "./components/Player";
+import { PlayerDetails } from "./components/PlayerDetails";
 import "./App.css";
-import { getPlayers, getPlayer } from "./api";
 
 function App() {
   const [players, setPlayers] = useState([]);
   const [player, setPlayer] = useState({});
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
     getPlayers().then((players) => {
@@ -16,37 +19,64 @@ function App() {
     getPlayer(playerId).then(setPlayer);
   }
 
+  function handlePlayerDelete(playerId) {
+    deletePlayer(playerId).then(() => {
+      getPlayers().then((players) => {
+        setPlayers(players);
+      });
+    });
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    const formData = new FormData(evt.target);
+    const newPlayer = Object.fromEntries(formData.entries());
+
+    createPlayer(newPlayer).then(() => {
+      getPlayers().then((players) => {
+        setPlayers(players);
+      });
+    });
+  }
+
+  function handleFilter(evt) {
+    setFilter(evt.target.value);
+  }
+
   return (
-    <>
+    <div onClick={() => setPlayer({})}>
       <h1>Puppy Bowl</h1>
+      <PlayerDetails player={player} />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" />
+        <label htmlFor="breed">Breed:</label>
+        <input type="text" name="breed" />
+        <button type="submit">Add Player</button>
+      </form>
+      <input type="text" name="filter" value={filter} onChange={handleFilter} />
       <table>
         <thead>
           <tr>
             <th>Name</th>
             <th>Breed</th>
-            <th>Status</th>
             <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {players.map((player) => {
             return (
-              <tr key={player.id}>
-                <td>{player.name}</td>
-                <td>{player.breed}</td>
-                <td>{player.status}</td>
-                <td>
-                  <button onClick={() => handlePlayerClick(player.id)}>
-                    View Player
-                  </button>
-                </td>
-              </tr>
+              <Player
+                key={player.id}
+                player={player}
+                onClick={handlePlayerClick}
+                onDelete={handlePlayerDelete}
+              />
             );
           })}
         </tbody>
       </table>
-      <dialog open={player.id}>{player.name}</dialog>
-    </>
+    </div>
   );
 }
 
---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+export function Player({ player, onClick, onDelete }) {
+  return (
+    <tr key={player.id}>
+      <td>{player.name}</td>
+      <td>{player.breed}</td>
+      <td>
+        <button onClick={() => onClick(player.id)}>View Player</button>
+        <button onClick={() => onDelete(player.id)}>Delete Player</button>
+      </td>
+    </tr>
+  );
+}
